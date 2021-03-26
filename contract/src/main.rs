3a51cdf46ba92e1ab49cc0d9588f8069d5d612ea
astro019 <dmitry.yakov7@gmail.com>
2021-03-27 02:49:19 +1000

    update casper_dsl

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -1,14 +1,15 @@
 #![no_main]
 use std::collections::BTreeSet;
-use types::{CLTyped, RuntimeArgs};
+use types::{
+    account::AccountHash,
+    contracts::{EntryPoint, EntryPointAccess, EntryPointType, EntryPoints},
+    runtime_args, CLType, CLTyped, Group, Parameter, RuntimeArgs, URef, U256,
+};
 
 mod dsl;
+use contract_macro::{casper_constructor, casper_contract, casper_method};
 use contract_vars::{Context, Save};
-use dsl::{
-    runtime, storage,
-    types::{account::AccountHash, U256},
-    GetKey, Map, UnwrapOrRevert, Variable,
-};
+use dsl::{runtime, storage, GetKey, Map, UnwrapOrRevert, Variable};
 
 #[derive(Context)]
 struct ERC20 {
@@ -16,11 +17,12 @@ struct ERC20 {
     token_symbol: Variable<String>,
     total_supply: Variable<U256>,
     balances: Map<AccountHash, U256>,
-    allowances: Map<(AccountHash, AccountHash), U256>
+    allowances: Map<(AccountHash, AccountHash), U256>,
 }
 
-// #[casper_contract]
+#[casper_contract]
 impl ERC20 {
+    #[casper_constructor]
     pub fn new(token_name: String, token_symbol: String, total_supply: U256) -> ERC20 {
         let mut erc20 = ERC20::default();
         erc20.balances.set(&runtime::get_caller(), total_supply);
@@ -29,35 +31,35 @@ impl ERC20 {
         erc20.token_symbol = Variable::new(String::from("token_symbol"), token_symbol);
         erc20
     }
-
+    #[casper_method]
     pub fn name(&self) -> String {
         self.token_name.get()
     }
-
+    #[casper_method]
     pub fn symbol(&self) -> String {
         self.token_symbol.get()
     }
-
+    #[casper_method]
     pub fn total_supply(&self) -> U256 {
         self.total_supply.get()
     }
-
-    pub fn balance_of(&self, address: &AccountHash) -> U256 {
-        self.balances.get(address)
+    #[casper_method]
+    pub fn balance_of(&self, address: AccountHash) -> U256 {
+        self.balances.get(&address)
     }
-
+    #[casper_method]
     pub fn transfer(&mut self, recipient: AccountHash, amount: U256) {
         self._transfer(runtime::get_caller(), recipient, amount)
     }
-
+    #[casper_method]
     pub fn allowance(&self, owner: AccountHash, spender: AccountHash) -> U256 {
         self.allowances.get(&(owner, spender))
     }
-
+    #[casper_method]
     pub fn approve(&mut self, spender: AccountHash, amount: U256) {
         self._approve(runtime::get_caller(), spender, amount);
     }
-
+    #[casper_method]
     pub fn transfer_from(&mut self, owner: AccountHash, recipient: AccountHash, amount: U256) {
         let spender = runtime::get_caller();
         self._transfer(owner, recipient, amount);
@@ -74,159 +76,4 @@ impl ERC20 {
     fn _approve(&mut self, owner: AccountHash, spender: AccountHash, amount: U256) {
         self.allowances.set(&(owner, spender), amount);
     }
-}
-
-// ----- Constructor -----
-#[no_mangle]
-pub extern "C" fn new() {
-    let token_name: String = runtime::get_named_arg("token_name");
-    let token_symbol: String = runtime::get_named_arg("token_symbol");
-    let total_supply: U256 = runtime::get_named_arg("total_supply");
-    let contract = ERC20::new(token_name, token_symbol, total_supply);
-    contract.save();
-}
-
-// ----- Public Methods -----
-#[no_mangle]
-pub extern "C" fn name() {
-    let contract = ERC20::default();
-    let result = contract.name();
-    runtime::ret(types::CLValue::from_t(result).unwrap());
-}
-
-#[no_mangle]
-pub extern "C" fn symbol() {
-    let contract = ERC20::default();
-    let result = contract.symbol();
-    runtime::ret(types::CLValue::from_t(result).unwrap());
-}
-
-#[no_mangle]
-pub extern "C" fn totalSupply() {
-    let contract = ERC20::default();
-    let result = contract.total_supply();
-    runtime::ret(types::CLValue::from_t(result).unwrap());
-}
-
-#[no_mangle]
-pub extern "C" fn balanceOf() {
-    let contract = ERC20::default();
-    let address: AccountHash = runtime::get_named_arg("address");
-    let result = contract.balance_of(&address);
-    runtime::ret(types::CLValue::from_t(result).unwrap());
-}
-
-#[no_mangle]
-pub extern "C" fn transfer() {
-    let mut contract = ERC20::default();
-    let recipient: AccountHash = runtime::get_named_arg("recipient");
-    let amount: U256 = runtime::get_named_arg("amount");
-    contract.transfer(recipient, amount);
-}
-
-#[no_mangle]
-pub extern "C" fn allowance() {
-    let contract = ERC20::default();
-    let owner: AccountHash = runtime::get_named_arg("owner");
-    let spender: AccountHash = runtime::get_named_arg("spender");
-    let result = contract.allowance(owner, spender);
-    runtime::ret(types::CLValue::from_t(result).unwrap());
-}
-
-#[no_mangle]
-pub extern "C" fn approve() {
-    let mut contract = ERC20::default();
-    let spender: AccountHash = runtime::get_named_arg("spender");
-    let amount: U256 = runtime::get_named_arg("amount");
-    contract.approve(spender, amount);
-}
-
-#[no_mangle]
-pub extern "C" fn transfer_from() {
-    let mut contract = ERC20::default();
-    let owner: AccountHash = runtime::get_named_arg("owner");
-    let recipient: AccountHash = runtime::get_named_arg("recipient");
-    let amount: U256 = runtime::get_named_arg("amount");
-    contract.transfer_from(owner, recipient, amount);
-}
-
-#[no_mangle]
-pub extern "C" fn call() {
-    let token_name: String = runtime::get_named_arg("token_name");
-    let token_symbol: String = runtime::get_named_arg("token_symbol");
-    let total_supply: U256 = runtime::get_named_arg("total_supply");
-
-    let (contract_package_hash, _) = storage::create_contract_package_at_hash();
-    let _constructor_access_uref: types::URef = storage::create_contract_user_group(
-        contract_package_hash,
-        "constructor_group",
-        1,
-        BTreeSet::new(),
-    )
-    .unwrap_or_revert()
-    .pop()
-    .unwrap_or_revert();
-    let constructor_group = types::Group::new("constructor_group");
-    let mut entry_points = types::EntryPoints::new();
-
-    entry_points.add_entry_point(types::EntryPoint::new(
-        String::from("new"),
-        vec![
-            types::Parameter::new("token_name", types::CLType::String),
-            types::Parameter::new("token_symbol", types::CLType::String),
-            types::Parameter::new("total_supply", types::CLType::U256),
-        ],
-        types::CLType::Unit,
-        types::EntryPointAccess::Groups(vec![constructor_group]),
-        types::EntryPointType::Contract,
-    ));
-
-    entry_points.add_entry_point(types::EntryPoint::new(
-        String::from("transfer"),
-        vec![
-            types::Parameter::new("recipient", AccountHash::cl_type()),
-            types::Parameter::new("amount", U256::cl_type()),
-        ],
-        types::CLType::Unit,
-        types::EntryPointAccess::Public,
-        types::EntryPointType::Contract,
-    ));
-
-    entry_points.add_entry_point(types::EntryPoint::new(
-        String::from("approve"),
-        vec![
-            types::Parameter::new("spender", AccountHash::cl_type()),
-            types::Parameter::new("amount", U256::cl_type()),
-        ],
-        types::CLType::Unit,
-        types::EntryPointAccess::Public,
-        types::EntryPointType::Contract,
-    ));
-
-    entry_points.add_entry_point(types::EntryPoint::new(
-        String::from("transfer_from"),
-        vec![
-            types::Parameter::new("owner", AccountHash::cl_type()),
-            types::Parameter::new("recipient", AccountHash::cl_type()),
-            types::Parameter::new("amount", U256::cl_type()),
-        ],
-        types::CLType::Unit,
-        types::EntryPointAccess::Public,
-        types::EntryPointType::Contract,
-    ));
-
-    let (contract_hash, _) =
-        storage::add_contract_version(contract_package_hash, entry_points, Default::default());
-    runtime::put_key("ERC20", contract_hash.into());
-    let contract_hash_pack = storage::new_uref(contract_hash);
-    runtime::put_key("ERC20_hash", contract_hash_pack.into());
-    runtime::call_contract::<()>(
-        contract_hash,
-        "new",
-        types::runtime_args! {
-            "token_name" => token_name,
-            "token_symbol" => token_symbol,
-            "total_supply" => total_supply
-        },
-    );
 }
--- contract_macro/src/lib.rs
+++ contract_macro/src/lib.rs
@@ -56,171 +56,56 @@ pub fn casper_context(input: TokenStream) -> TokenStream {
     gen.into()
 }
 
-// The main macro that sets up the deploy function for the contract
-// It loads the module of a given name and then iterates over the content of the
-// module
-#[proc_macro_attribute]
-pub fn casper_contract(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    let item: syn::ItemMod = syn::parse_macro_input!(input);
-    let name = &item.ident;
-    let mut deploy_args = proc_macro2::TokenStream::new();
-    let mut deploy_def = proc_macro2::TokenStream::new();
-    let mut func_def = proc_macro2::TokenStream::new();
-    match item.content {
-        Some(module_content) => {
-            let (deploy_def_content, func_def_content, deploy_args_content) =
-                get_entry_points(name, module_content.1).unwrap();
-            deploy_def.extend(deploy_def_content);
-            func_def.extend(func_def_content);
-            deploy_args.extend(deploy_args_content);
-        }
-        None => println!("Empty"),
-    };
-    let gen = quote! {
-        fn __deploy( #deploy_args ) {
-            #deploy_def
-        }
-        #func_def
-        fn ret<T: CLTyped + ToBytes>(value: T) {
-            runtime::ret(CLValue::from_t(value).unwrap_or_revert())
-        }
-    };
-    // Return the deploy function along with the call function
-    gen.into()
-}
-
-#[proc_macro_attribute]
-pub fn casper_initiator(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    // An optional macro that loads the initating function for the contract which returns the BTreeMap
-    // The macro itself does not return the BTreeMap
-    let item: syn::ItemFn = syn::parse_macro_input!(input);
-    let func_def = quote! { #item };
-    let gen = quote! {
-        #func_def
-    };
-    // Return the function defintion for the intiating function
-    gen.into()
-}
-
 #[proc_macro_attribute]
 pub fn casper_constructor(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    // The macro responsible for generating the constructor
-    let mut item: syn::ItemFn = syn::parse_macro_input!(input);
-    let mut input_strings: Vec<String> = Vec::new();
-    let mut declaration = proc_macro2::TokenStream::new();
-    let orignal_ident = item.sig.ident.clone();
-    let name = &orignal_ident;
-    let new_ident = Ident::new(&format!("__{}", name), name.span());
-    item.sig.ident = new_ident;
-    let internal = &item.sig.ident;
-    let constructor_def = quote! { #item };
-    if item.sig.inputs.is_empty() {
-        let gen = quote! {
-            #[no_mangle]
-            fn call() {
-                __deploy();
-            }
-            #constructor_def
-            #[no_mangle]
-            fn #name() {
-                #internal()
-            }
-        };
-        return gen.into();
-    }
-    for indiviual_input in item.sig.inputs {
-        let (dec, arg, _, _) = get_var_declaration(&indiviual_input);
-        declaration.extend(dec);
-        input_strings.push(arg);
-    }
-    let input_args = prep_input(input_strings);
-    let gen = quote! {
-        #[no_mangle]
-        fn call() {
-            #declaration
-            __deploy(#input_args)
-        }
-        #constructor_def
-        #[no_mangle]
-        fn #name() {
-            #declaration
-            #internal(#input_args)
-        }
-    };
-    // Return the function defintion for the constructor function
+    let item: syn::ImplItemMethod = syn::parse_macro_input!(input);
+    let gen = quote! { #item };
     gen.into()
 }
 
 #[proc_macro_attribute]
 pub fn casper_method(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    let mut item: syn::ItemFn = syn::parse_macro_input!(input);
-    let orignal_ident = item.sig.ident.clone();
-    let return_type = item.sig.output.clone();
-    let mut return_code = proc_macro2::TokenStream::new();
-    let mut return_func = proc_macro2::TokenStream::new();
-    if let syn::ReturnType::Type(_arrow, rt) = return_type {
-        if let syn::Type::Path(path) = *rt {
-            let return_ident = &path.path.segments[0].ident;
-            let code = quote! { let val: #return_ident = };
-            return_code.extend(code);
-        }
-    }
-    if !return_code.is_empty() {
-        let runtime_return = quote! { ret(val) };
-        return_func.extend(runtime_return)
-    }
-    let name = &orignal_ident;
-    let new_ident = Ident::new(&format!("__{}", name), name.span());
-    item.sig.ident = new_ident;
-    let internal = &item.sig.ident;
-    let func_def = quote! { #item };
-    let mut declaration = proc_macro2::TokenStream::new();
-    let mut input_strings: Vec<String> = Vec::new();
-    if item.sig.inputs.is_empty() {
-        let gen = quote! {
-            #func_def
-            #[no_mangle]
-            fn #name() {
-                #declaration
-                #return_code #internal();
-                #return_func
-            }
-        };
-        return gen.into();
-    }
-    for indiviual_input in item.sig.inputs {
-        let (dec, arg, _, _) = get_var_declaration(&indiviual_input);
-        declaration.extend(dec);
-        input_strings.push(arg);
-    }
-    let input_args = prep_input(input_strings);
+    let item: syn::ItemFn = syn::parse_macro_input!(input);
     let gen = quote! {
-        #func_def
-        #[no_mangle]
-        fn #name()  {
-            #declaration
-            #return_code #internal(#input_args);
-            #return_func
-        }
+        #item
     };
-    // Return both the entry point and the function defintion
-    // #[no_mangle]
-    // fn bar() {
-    //     __bar()
-    // }
-    //  fn __bar() {}
     gen.into()
 }
 
-// Constructs a new entry point that can be inserted into
-// the deploy function.
+// The main macro that sets up the deploy function for the contract
+// It loads the module of a given name and then iterates over the content of the
+// module
+#[proc_macro_attribute]
+pub fn casper_contract(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let item: syn::ItemImpl = syn::parse_macro_input!(input);
+    let name = Ident::new("ERC20", Span::call_site());
+    let mut deploy_def = proc_macro2::TokenStream::new();
+    let mut method_def = proc_macro2::TokenStream::new();
+    let (deploy_def_content, method_def_content) = 
+        get_entry_points(&name, item.items.clone()).unwrap();
+    deploy_def.extend(deploy_def_content);
+    method_def.extend(method_def_content);
+
+    let impl_def = quote! { #item };
+
+    let gen = quote! {
+        #impl_def
+        #method_def
+        #[no_mangle]
+        pub extern "C" fn call() {
+            #deploy_def
+        }
+    };
+
+    gen.into()
+}
+
 fn get_entry_points(
     name: &syn::Ident,
-    funcs: Vec<syn::Item>,
+    funcs: Vec<syn::ImplItem>
 ) -> Option<(
     proc_macro2::TokenStream,
-    proc_macro2::TokenStream,
-    proc_macro2::TokenStream,
+    proc_macro2::TokenStream
 )> {
     // The initial empty defintion Tokenstream that will be incrementally added to with various parts of the deploy function
     let mut definitions: proc_macro2::TokenStream = proc_macro2::TokenStream::new();
@@ -229,14 +114,14 @@ fn get_entry_points(
         let (contract_package_hash, _) = storage::create_contract_package_at_hash();
         let _constructor_access_uref: URef = storage::create_contract_user_group(
             contract_package_hash,
-            "constructor",
+            "constructor_group",
             1,
             BTreeSet::new(),
         )
         .unwrap_or_revert()
         .pop()
         .unwrap_or_revert();
-        let constructor_group = Group::new("constructor");
+        let constructor_group = Group::new("constructor_group");
         let mut entry_points = EntryPoints::new();
     };
     // The two empty streams for arguments that must be passed to the deploy function and the content of the contract call respectively
@@ -247,7 +132,7 @@ fn get_entry_points(
     let constructor_ident = Ident::new("casper_constructor", Span::call_site());
     // The intiating function must also be idenitfied so that the function can be placed in the deploy function
     let member_ident = Ident::new("casper_initiator", Span::call_site());
-    let mut init_ident: proc_macro2::TokenStream = quote! { Default::default() };
+    let init_ident: proc_macro2::TokenStream = quote! { Default::default() };
     // Empty token stream that will change depending on the type of function passed to it (constructor/method)
     let mut access_token: proc_macro2::TokenStream;
     // Loop over every possible function and match to see if it is indeed a function, the statment could be a literal like
@@ -255,15 +140,10 @@ fn get_entry_points(
     //  For every #[casper_methods] in the contract module
     let mut constructor_presence: bool = false;
     for func in funcs {
-        if let syn::Item::Fn(func_body) = func {
-            // If the function is not a constructor or method it will have no attributes and we must ignore that function
-            // Additionally, the function could also be an intiatior, which it means it should be ignored as well.
-            if func_body.attrs.is_empty() {
-                let gen = quote! { #func_body };
-                definitions.extend(gen);
-            } else if !func_body.attrs.is_empty()
-                && member_ident != func_body.attrs[0].path.segments[0].ident
-            {
+        // If the function is not a constructor or method it will have no attributes and we must ignore that function
+        // Additionally, the function could also be an intiatior, which it means it should be ignored as well.
+        if let syn::ImplItem::Method(func_body) = func {
+            if !func_body.attrs.is_empty() && member_ident != func_body.attrs[0].path.segments[0].ident {
                 //  Identify if the function is a constructor or a regular method
                 if constructor_ident == func_body.attrs[0].path.segments[0].ident {
                     constructor_presence = true;
@@ -274,11 +154,11 @@ fn get_entry_points(
                 } else {
                     //  EntryAccessPoint for a regular casper_method
                     access_token = quote! { EntryPointAccess::Public };
+                    let definition_content = get_method(&name, &func_body);
+                    definitions.extend(definition_content);
                 }
-                let name = &func_body.sig.ident;
-                let def = quote! { #func_body };
-                definitions.extend(def);
-                let string_name = format!("{}", name);
+                let func_name = &func_body.sig.ident;
+                let string_name = format!("{}", func_name);
                 let mut arg: proc_macro2::TokenStream = proc_macro2::TokenStream::new();
                 if !func_body.sig.inputs.is_empty() {
                     for input in func_body.sig.inputs {
@@ -303,20 +183,19 @@ fn get_entry_points(
                     ));
                 };
                 init.extend(gen)
-            } else if member_ident == func_body.attrs[0].path.segments[0].ident
-                && !func_body.attrs.is_empty()
-            {
-                // If it is the intitator function, then set the identifier with the name of the specified function
-                // Return the function definition
-                let init_name = func_body.sig.ident.clone();
-                let def = quote! { #func_body };
-                definitions.extend(def);
-                init_ident = quote! { #init_name() };
-            } else if func_body.attrs.is_empty() {
-                // Final path to ensure all non-associated functions are included within the contract
-                let def = quote! { #func_body };
-                definitions.extend(def);
             }
+            // else if member_ident == func_body.attrs[0].path.segments[0].ident && !func_body.attrs.is_empty()
+            // {
+            //     // If it is the intitator function, then set the identifier with the name of the specified function
+            //     // Return the function definition
+            //     let init_name = func_body.sig.ident.clone();
+            //     let def = quote! { #func_body };
+            //     definitions.extend(def);
+            //     init_ident = quote! { #init_name() };
+            // } else if func_body.attrs.is_empty() {
+            //     let gen = quote! { #func_body };
+            //     definitions.extend(gen);
+            // } 
         }
         // Simply pass over all other literals
     }
@@ -336,11 +215,24 @@ fn get_entry_points(
     init.extend(tail);
     // Finally once all fragments of the deploy function, defintions for all other functions and the arguments for the deploy function have been aggregated
     // Return the indiviual pieces
-    Some((init, definitions, deploy_args))
+    Some((init, definitions))
+}
+
+#[proc_macro_attribute]
+pub fn casper_initiator(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    // An optional macro that loads the initating function for the contract which returns the BTreeMap
+    // The macro itself does not return the BTreeMap
+    let item: syn::ItemFn = syn::parse_macro_input!(input);
+    let func_def = quote! { #item };
+    let gen = quote! {
+        #func_def
+    };
+    // Return the function defintion for the intiating function
+    gen.into()
 }
 
 // Construct the content for the contract call to be placed in the deploy method
-fn get_call(init: &syn::ItemFn) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+fn get_call(init: &syn::ImplItemMethod) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
     // The name of the function that must be called
     let name = &init.sig.ident.clone();
     // Generate the string represenation of the function name
@@ -350,12 +242,13 @@ fn get_call(init: &syn::ItemFn) -> (proc_macro2::TokenStream, proc_macro2::Token
     // The call_contract method requires all the arguments that the constructor function
     // takes. Thus we create a new empty token stream for it
     let mut args: proc_macro2::TokenStream = proc_macro2::TokenStream::new();
+    let mut decs: proc_macro2::TokenStream = proc_macro2::TokenStream::new();
     // For every argument in the arguments of the function defintion
     // Loop over and fill both token streams
     for indiviual_input in &init.sig.inputs {
         // Retrieve the arguments, the name for each argument, and the type
         //  _, somearg, "somearg", type_of_arg
-        let (_, arg, arg_name, arg_type) = get_var_declaration(&indiviual_input);
+        let (dec, arg, arg_name, arg_type) = get_var_declaration(&indiviual_input);
         // Create an identifier for the argument
         let arg_ident = Ident::new(arg.as_str(), Span::call_site());
         let temp = quote! { #arg => #arg_ident, };
@@ -364,15 +257,17 @@ fn get_call(init: &syn::ItemFn) -> (proc_macro2::TokenStream, proc_macro2::Token
         args.extend(temp);
         // Fill the arguments for the deploy function
         input_for_deploy.extend(input_dec);
+        decs.extend(dec);
     }
     // Generate the code forthe call_contract function call
     let gen = quote! {
+        #decs
         runtime::call_contract::<()>(
             contract_hash,
             #string_name,
             runtime_args! {
                 #args
-            },
+            }
         );
     };
     // Return the call_contract code, along with the inputs for the deploy
@@ -382,44 +277,79 @@ fn get_call(init: &syn::ItemFn) -> (proc_macro2::TokenStream, proc_macro2::Token
 fn get_param(arg: &FnArg) -> proc_macro2::TokenStream {
     //Generate a TokenSteam for each indiviual parameter for indiviual args passed to the functions
     let mut cl_type_declaration = proc_macro2::TokenStream::new();
-    let arg_name: &syn::Ident;
+    let arg_name: syn::Ident;
     match arg {
         FnArg::Typed(ref pat_type) => {
             match *pat_type.pat {
                 syn::Pat::Ident(ref pat_ident) => {
-                    arg_name = &pat_ident.ident;
+                    arg_name = pat_ident.ident.clone();
                 }
                 _ => panic!("Incorrect"),
             }
             cl_type_declaration.extend(get_parameter(&pat_type.ty));
         }
-        _ => panic!("{:?}", arg),
+        _ => {
+            arg_name = Ident::new("Self", Span::call_site());
+        }
     }
     let string_arg = format!("{}", arg_name);
-    let declaration = quote! {Parameter::new(#string_arg, #cl_type_declaration),};
+    let mut declaration = quote! {Parameter::new(#string_arg, #cl_type_declaration),};
+    if string_arg == "Self" {
+        declaration = quote! {};
+    }
     declaration
 }
 
-// Generate the inputs for the function call
-fn prep_input(input_strings: Vec<String>) -> proc_macro2::TokenStream {
-    // When a function must be called we must create an identifier foe each of the args,
-    // seperated by a comma
-    let first_arg = Ident::new(&input_strings[0], Span::call_site());
-    let mut args = quote! { #first_arg, };
-    //
-    for input_string in input_strings.iter().take(input_strings.len() - 1).skip(1) {
-        //Create an Ident from the string representation of each of the args passed to the function
-        let ident = Ident::new(&input_string, Span::call_site());
-        let temp = quote! { #ident, };
-        args.extend(temp);
+fn get_method(name: &syn::Ident, item: &syn::ImplItemMethod) -> proc_macro2::TokenStream {
+    let item_ident = item.sig.ident.clone();
+    let return_type = item.sig.output.clone();
+    let mut func_body = proc_macro2::TokenStream::new();
+    let mut func_return = proc_macro2::TokenStream::new();
+
+    let mut declaration = proc_macro2::TokenStream::new();
+    let mut input_strings: Vec<String> = Vec::new();
+    for indiviual_input in &item.sig.inputs {
+        match indiviual_input {
+            FnArg::Typed(_) => {
+                let (dec, arg, _, _) = get_var_declaration(&indiviual_input);
+                declaration.extend(dec);
+                input_strings.push(arg);
+            }
+            _ => {}
+        };
     }
-    if input_strings.len() == 1 {
-        return args;
+    let string_input_arg = input_strings.join(",");
+    let input_args = string_input_arg.parse::<::proc_macro2::TokenStream>().unwrap();
+
+    if let syn::ReturnType::Type(_arrow, rt) = return_type {
+        if let syn::Type::Path(_) = *rt {
+            let code = quote! { 
+                let contract = #name::default();
+                #declaration
+                let result = contract.#item_ident(#input_args);
+            };
+            func_body.extend(code);
+            if !func_body.is_empty() {
+                let runtime_return = quote! { runtime::ret(types::CLValue::from_t(result).unwrap()); };
+                func_return.extend(runtime_return)
+            }
+        }
+    } else {
+        let code = quote! { 
+            let mut contract = #name::default();
+            #declaration
+            contract.#item_ident(#input_args);
+        };
+        func_body.extend(code);
     }
-    let last_ident = Ident::new(&input_strings[input_strings.len() - 1], Span::call_site());
-    let last_arg = quote! { #last_ident };
-    args.extend(last_arg);
-    args
+    let gen = quote! {
+        #[no_mangle]
+        pub extern "C" fn #item_ident() {
+            #func_body
+            #func_return
+        }
+    };
+    gen
 }
 
 // If an function has the declaration: bar(a: u64, b: String)
