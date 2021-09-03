4d9228d3eedea7d19e36cfc280c491e61f9aadbc
Maciej Zielinski <maciek.s.zielinski@gmail.com>
2021-09-03 14:26:11 +0200

    Override example

--- erc20/bin/erc20_token.rs
+++ erc20/bin/erc20_token.rs
@@ -1,11 +1,16 @@
 #![no_main]
 
-use contract_utils::BlockchainContext;
+use contract_utils::{BlockchainContext, Context};
 use erc20::{self, set_erc20_instance, SimpleERC20};
 
 struct SimpleToken;
 impl BlockchainContext for SimpleToken {}
-impl SimpleERC20 for SimpleToken {}
+impl SimpleERC20 for SimpleToken {
+    // Override example.
+    fn transfer(&self, recipient: casper_types::Key, amount: casper_types::U256) {
+        self.make_transfer(self.get_caller(), recipient, amount);
+    }
+}
 
 #[no_mangle]
 fn init_erc20() {
