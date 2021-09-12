280be9911793277e4b9806603e9301d6d70de8de
Michał Papierski <michal@casperlabs.io>
2021-09-12 14:28:44 +0200

    s/balance/supply

--- erc20/src/total_supply.rs
+++ erc20/src/total_supply.rs
@@ -10,12 +10,12 @@ pub(crate) fn total_supply_uref() -> URef {
     detail::get_uref(TOTAL_SUPPLY_KEY_NAME)
 }
 
-/// Reads a total balance from a specified [`URef`].
+/// Reads a total supply from a specified [`URef`].
 pub(crate) fn read_total_supply_from(uref: URef) -> U256 {
     storage::read(uref).unwrap_or_revert().unwrap_or_revert()
 }
 
-/// Writes a total balance to a specific [`URef`].
+/// Writes a total supply to a specific [`URef`].
 pub(crate) fn write_total_supply_to(uref: URef, value: U256) {
     storage::write(uref, value);
 }
