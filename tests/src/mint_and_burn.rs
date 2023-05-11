922f77667e1c536890710e1cfcf7e7fe6fd7fff6
Deuszex <lovaszoltanr@gmail.com>
2023-05-11 16:48:48 +0200

    change_security test

--- tests/src/mint_and_burn.rs
+++ tests/src/mint_and_burn.rs
@@ -1,12 +1,14 @@
 use casper_engine_test_support::{ExecuteRequestBuilder, DEFAULT_ACCOUNT_ADDR};
-use casper_types::{runtime_args, ApiError, Key, RuntimeArgs, U256, system::MINT};
+use casper_types::{runtime_args, system::MINT, ApiError, Key, RuntimeArgs, U256};
 
 use crate::utility::{
     constants::{
-        AMOUNT, ARG_AMOUNT, ARG_DECIMALS, ARG_NAME, ARG_OWNER, ARG_SYMBOL, ARG_TOTAL_SUPPLY,
-        ERROR_INSUFFICIENT_BALANCE, ERROR_OVERFLOW, METHOD_BURN, METHOD_MINT, OWNER,
-        TOKEN_DECIMALS, TOKEN_NAME, TOKEN_OWNER_ADDRESS_1, TOKEN_OWNER_ADDRESS_2,
-        TOKEN_OWNER_AMOUNT_1, TOKEN_OWNER_AMOUNT_2, TOKEN_SYMBOL, TOKEN_TOTAL_SUPPLY, ENABLE_MINT_BURN, MINTER_LIST, ACCOUNT_1_ADDR, BURNER_LIST, MINT_AND_BURN_LIST,
+        ACCOUNT_1_ADDR, ADMIN_LIST, AMOUNT, ARG_AMOUNT, ARG_DECIMALS, ARG_NAME, ARG_OWNER,
+        ARG_SYMBOL, ARG_TOTAL_SUPPLY, BURNER_LIST, CHANGE_SECURITY, ENABLE_MINT_BURN,
+        ERROR_INSUFFICIENT_BALANCE, ERROR_OVERFLOW, METHOD_BURN, METHOD_MINT, MINTER_LIST,
+        MINT_AND_BURN_LIST, NONE_LIST, OWNER, TOKEN_DECIMALS, TOKEN_NAME, TOKEN_OWNER_ADDRESS_1,
+        TOKEN_OWNER_ADDRESS_2, TOKEN_OWNER_AMOUNT_1, TOKEN_OWNER_AMOUNT_2, TOKEN_SYMBOL,
+        TOKEN_TOTAL_SUPPLY,
     },
     installer_request_builders::{
         cep18_check_balance_of, cep18_check_total_supply, setup_with_args, TestContext,
@@ -241,7 +243,6 @@ fn test_should_not_burn_above_balance() {
     );
 }
 
-
 #[test]
 fn test_should_not_mint_or_burn_with_entrypoint_disabled() {
     let mint_amount = U256::one();
@@ -293,7 +294,6 @@ fn test_should_not_mint_or_burn_with_entrypoint_disabled() {
         "{:?}",
         error
     );
-
 }
 
 #[test]
@@ -374,7 +374,7 @@ fn test_security_minter_rights() {
     .build();
 
     builder.exec(mint_request).commit().expect_success();
-    
+
     let burn_request = ExecuteRequestBuilder::contract_call_by_hash(
         *ACCOUNT_1_ADDR,
         cep18_token,
@@ -495,4 +495,53 @@ fn test_security_mint_and_burn_rights() {
     .build();
 
     builder.exec(burn_request).commit().expect_success();
+}
+
+#[test]
+fn test_change_security() {
+    let mint_amount = U256::one();
+
+    let (mut builder, TestContext { cep18_token, .. }) = setup_with_args(runtime_args! {
+        ARG_NAME => TOKEN_NAME,
+        ARG_SYMBOL => TOKEN_SYMBOL,
+        ARG_DECIMALS => TOKEN_DECIMALS,
+        ARG_TOTAL_SUPPLY => U256::from(TOKEN_TOTAL_SUPPLY),
+        ENABLE_MINT_BURN => true,
+        ADMIN_LIST => vec![Key::Account(*ACCOUNT_1_ADDR)]
+    });
+
+    let change_security_request = ExecuteRequestBuilder::contract_call_by_hash(
+        *ACCOUNT_1_ADDR,
+        cep18_token,
+        CHANGE_SECURITY,
+        runtime_args! {
+            NONE_LIST => vec![Key::Account(*DEFAULT_ACCOUNT_ADDR)],
+        },
+    )
+    .build();
+
+    builder
+        .exec(change_security_request)
+        .commit()
+        .expect_success();
+
+    let mint_request = ExecuteRequestBuilder::contract_call_by_hash(
+        *DEFAULT_ACCOUNT_ADDR,
+        cep18_token,
+        METHOD_MINT,
+        runtime_args! {
+            ARG_OWNER => TOKEN_OWNER_ADDRESS_1,
+            ARG_AMOUNT => mint_amount,
+        },
+    )
+    .build();
+
+    builder.exec(mint_request).commit();
+
+    let error = builder.get_error().expect("should have error");
+    assert!(
+        matches!(error, CoreError::Exec(ExecError::Revert(ApiError::User(user_error))) if user_error == 60010),
+        "{:?}",
+        error
+    );
 }
