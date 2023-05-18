bbe7dd98fb10b2b2fa0b4a4fef6d76edaf96b686
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2023-05-18 15:34:22 +0900

    update js client error code

--- client-js/src/error.ts
+++ client-js/src/error.ts
@@ -1,10 +1,27 @@
-const MAX_U16 = 65535;
-
 export enum ERROR_CODES {
-  ERROR_INVALID_CONTEXT = MAX_U16,
-  ERROR_INSUFFICIENT_BALANCE = MAX_U16 - 1,
-  ERROR_INSUFFICIENT_ALLOWANCE = MAX_U16 - 2,
-  ERROR_OVERFLOW = MAX_U16 - 3
+  /// CEP18 contract called from within an invalid context.
+  InvalidContext = 60000,
+  /// Spender does not have enough balance.
+  InsufficientBalance = 60001,
+  /// Spender does not have enough allowance approved.
+  InsufficientAllowance = 60002,
+  /// Operation would cause an integer overflow.
+  Overflow = 60003,
+  PackageHashMissing = 60004,
+  PackageHashNotPackage = 60005,
+  InvalidEventsMode = 60006,
+  MissingEventsMode = 60007,
+  Phantom = 60008,
+  FailedToGetArgBytes = 60009,
+  InsufficientRights = 60010,
+  InvalidAdminList = 60011,
+  InvalidMinterList = 60012,
+  InvalidBurnerList = 60013,
+  InvalidMintAndBurnList = 60014,
+  InvalidNoneList = 60015,
+  InvalidEnableMBFlag = 60016,
+  AlreadyInitialized = 60017,
+  MintBurnDisabled = 60018
 }
 
 export class ContractError extends Error {
